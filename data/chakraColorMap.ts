6c9ce716b0c93012e0a0a641bcb46ea73e2083f0
NRoseDev <nicholer001@gmail.com>
2026-04-30 15:15:49 -0500

    Create chakraColorMap.ts

---
+++ data/chakraColorMap.ts
@@ -0,0 +1,9 @@
+export const chakraColorMap = {
+  root: ["#ff3b30"],
+  sacral: ["#ff9500"],
+  solar: ["#ffcc00"],
+  heart: ["#34c759"],
+  throat: ["#007aff"],
+  thirdEye: ["#5856d6"],
+  crown: ["#af52de"]
+};
